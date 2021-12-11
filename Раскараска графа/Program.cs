9ecfcc99b4c9ec3e0781842d32fb8678e764312e
Ivan <ivan.rok2001@mail.ru>
2021-12-11 15:35:05 +0500

    Реализован Жадный Алгоритм расскраски графа

--- Раскараска графа/Program.cs
+++ Раскараска графа/Program.cs
@@ -1,38 +1,38 @@
 ﻿using System;
+using Раскараска_графа;
 namespace ColoringGraph
 {
     public class Graph
     {
-        public bool[,] AdjMatrix;
-        private int NumVertices;
-
+        public bool[,] AdjMatrix;//Матрица смежности
+        private int NumVertices;//Кол-во вершин
         public Graph(int NumVerticies)
         {
 
             NumVertices = NumVerticies;
             AdjMatrix = new bool[NumVertices, NumVertices];
         }
-        public void AddEdge(int i, int j)
+        public void AddEdge(int i, int j)//Добавление ребра
         {
             AdjMatrix[i, j] = true;
             AdjMatrix[j, i] = true;
         }
-        public void PrintMatrix()
+        public void PrintMatrix()//Вывод матрицы
         {
             for (int i = 0; i < AdjMatrix.GetLength(0); i++)
             {
-                Console.Write($"{i}: ");
+                Console.Write(i > 9 ? $"{i}:   " : $" {i}:   ");
                 for (int j = 0; j < AdjMatrix.GetLength(1); j++)
                 {
                     Console.Write($"{(AdjMatrix[i, j] ? 1 : 0)} ");
                 }
-                Console.WriteLine();    
+                Console.WriteLine();
             }
         }
     }
     public static class Extensions
     {
-        public static T[,] ToRectangularArray<T>(this IReadOnlyList<T[]> arrays)
+        public static T[,] ToRectangularArray<T>(this IReadOnlyList<T[]> arrays)//Метод расширения для удобного считывания из файла
         {
             var ret = new T[arrays.Count, arrays[0].Length];
             for (var i = 0; i < arrays.Count; i++)
@@ -43,34 +43,79 @@ namespace ColoringGraph
     }
     class Program
     {
+        private static void AlgorithmSelection(bool[,] AdjMatrix)
+        {
+            int k;
+            Console.WriteLine("Выберите алгоритм раскраски: " +
+                               "\nПолный перебор - введите '0'" +
+                               "\nЖадный - введите '1'" +
+                               "\nЖадный оптимизированный - введите '2'" +
+                               "\nЗакончить - введите -1");
+            k = Convert.ToInt32(Console.ReadLine());
+            switch (k)
+            {
+                case 0:
+                    {
+
+                        break;
+                    }
+                case 1:
+                    {
+                        TrivialGreedy coloring = new TrivialGreedy(AdjMatrix);
+                        coloring.Coloring();
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
         static void Main(string[] Args)
         {
-            int s,x;
+            int s, x;
             Random rnd = new Random(DateTime.Now.Millisecond);
             Graph graph;
-            Console.WriteLine("Если вы хотите считать матрицу смежности из файла введите - '0', сгенерировать - 'кол-во вершин'");
+            Console.WriteLine("Выберите способ задания матрицы: " +
+                               "\nИз файла - введите '0'" +
+                               "\n Сгенерировать - введите 'кол-во вершин'" +
+                               "\n Закончить работу - введите -1");
             s = Convert.ToInt32(Console.ReadLine());
             switch (s)
             {
-                case 0:
-                    {                        
+                case 0://Считывание матрицв из файла
+                    {
                         graph = new Graph(0);
                         graph.AdjMatrix = File.ReadAllLines("MyAdjMatrix.txt").Select(x => x.Split(' ')
                                                                               .Select(int.Parse).Select(x => (x == 1 ? true : false))
                                                                               .ToArray()).ToArray().ToRectangularArray();
                         graph.PrintMatrix();
-                        break; 
+                        AlgorithmSelection(graph.AdjMatrix);
+                        break;
                     }
-                default:
+                case -1://Выход
+                    {
+                        break;
+                    }
+                default://Генерация матрицы
                     {
                         graph = new Graph(s);
                         for (int i = 0; i < s; i++)
-                            for (int j = 0; j < s; j++)
+                            for (int j = i; j < s; j++)
                             {
                                 x = rnd.Next(0, 2);
-                                if (x == 1) graph.AddEdge(i, j);
+                                if (x == 1)
+                                {
+                                    graph.AddEdge(i, j);
+                                    graph.AddEdge(j, i);
+                                }
                             }
-                        graph.PrintMatrix();       
+                        graph.PrintMatrix();
+                        AlgorithmSelection(graph.AdjMatrix);
                         break;
                     }
 
--- Раскараска графа/TrivialGreedy.cs
+++ Раскараска графа/TrivialGreedy.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Раскараска_графа
+﻿namespace Раскараска_графа
 {
     internal class TrivialGreedy
     {
+        private bool[,] AdjMatrix;//Матрица смежности
+        public TrivialGreedy(bool[,] AdjMatrix)
+        {
+            this.AdjMatrix = AdjMatrix;
+        }
+        public void Coloring()//Раскраска Графа
+        {
+          int[] colors = new int[AdjMatrix.GetLength(0)]; //Массив цветов
+          Array.Fill(colors, -1);//Инициализация всех вершин без цвета
+          colors[0] = 0;//Назначение первой вершине первого цвета
+
+          //Временный массив для хранения доступных цветов.
+          //Ложное значение available[cr] будет означать,
+          //что цвет cr назначен одной из соседних вершин.
+          bool[] available = new bool[AdjMatrix.GetLength(0)];
+
+          Array.Fill(available, true);//Все цвета доступны
+
+          //Назначьте цвета оставшимся вершинам V - 1
+          for (int i = 1; i < AdjMatrix.GetLength(0); i++)
+          {
+              //Обрабатываем все соседние вершины и помечаем их цвета как недоступные
+              for (int j = 0; j < AdjMatrix.GetLength(0); j++)
+                  if (colors[AdjMatrix[i, j] ? 1 : 0] != -1)
+                      available[colors[AdjMatrix[i, j] ? 1 : 0]] = false;
+
+              //Поиск первого доступного цвета
+              int cr;
+              for (cr = 0; cr < AdjMatrix.GetLength(0); cr++)
+                  if (available[cr]) break;
+
+              colors[i] = cr;//Назначение найденого цвета
+              Array.Fill(available, true);// сброс доступных цветов для следущей вершины               
+          }
+          //Вывод вершин и соответсвующего цвета
+          for(int i = 0;i < AdjMatrix.GetLength(0); i++)
+                Console.WriteLine($"Вершина {i} ---> Цвет {colors[i]}");
+        }
     }
 }
